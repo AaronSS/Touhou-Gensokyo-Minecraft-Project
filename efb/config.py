5c6839a2ef0b23bf75e250aabd874cad0ba693bf
root <root@instance-2.c.sigma-heuristic-210909.internal>
2018-12-20 00:05:39 +0000

    update

---
+++ efb/config.py
@@ -0,0 +1,13 @@
+master_channel = 'plugins.eh_telegram_master', 'TelegramChannel'
+slave_channels = [('plugins.eh_wechat_slave', 'WeChatChannel')]
+ 
+eh_telegram_master = {
+    "token": "765605750:AAGsBLoH2FcCGm4GBK0zzmlt6Lr9E68Ry6g",
+    "admins": [492853456],
+    "bing_speech_api": ["xxx", "xxx"],
+    "baidu_speech_api": {
+        "app_id": 0,
+        "api_key": "xxx",
+        "secret_key": "xxx"
+    }
+}
